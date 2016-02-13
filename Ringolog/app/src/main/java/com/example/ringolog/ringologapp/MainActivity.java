47c4d6c5d1caff251fa34dc88db5af6a6e6597cf
Philippe Le <philippele_1993@hotmail.com>
2016-02-13 05:02:25 -0500

    interaction avec le backend

--- Ringolog/app/src/main/java/com/example/ringolog/ringologapp/MainActivity.java
+++ Ringolog/app/src/main/java/com/example/ringolog/ringologapp/MainActivity.java
@@ -1,18 +1,58 @@
 package com.example.ringolog.ringologapp;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.firebase.client.AuthData;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+    /* TextView that is used to display information about the logged in user */
+    private TextView mLoggedInStatusTextView;
 
+    /* A dialog that is presented until the Firebase authentication finished. */
+    private ProgressDialog mAuthProgressDialog;
+
+    /* A reference to the Firebase */
+    private Firebase mFirebaseRef;
+
+    /* Data from the authenticated user */
+    private AuthData mAuthData;
+
+    /* Listener for Firebase session changes */
+    private Firebase.AuthStateListener mAuthStateListener;
+
+    /* *************************************
+ *              PASSWORD               *
+ ***************************************/
+    private Button mPasswordLoginButton;
+    public class User {
+        private int birthYear;
+        private String fullName;
+        public User() {}
+        public User(String fullName, int birthYear) {
+            this.fullName = fullName;
+            this.birthYear = birthYear;
+        }
+        public long getBirthYear() {
+            return birthYear;
+        }
+        public String getFullName() {
+            return fullName;
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +61,40 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         Firebase.setAndroidContext(this);
+
+        // Getters
+        mFirebaseRef = new Firebase(getResources().getString(R.string.firebase_url));
+        mFirebaseRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                System.out.println(snapshot.getValue());
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+            }
+        });
+        /* authentification */
+       /* mFirebaseRef = new Firebase(getResources().getString(R.string.firebase_url));
+
+        mFirebaseRef.authWithPassword("philippe@firebase.com", "philippe", new Firebase.AuthResultHandler() {
+            @Override
+            public void onAuthenticated(AuthData authData) {
+                System.out.println("User ID: " + authData.getUid() + ", Provider: " + authData.getProvider());
+            }
+
+            @Override
+            public void onAuthenticationError(FirebaseError firebaseError) {
+                System.out.println("error");
+                // there was an error
+            }
+        });*/
+        /* setter
+        Firebase alanRef = mFirebaseRef.child("users").child("user2");
+        User alan = new User("George Lord", 1930);
+        alanRef.setValue(alan);
+        */
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
